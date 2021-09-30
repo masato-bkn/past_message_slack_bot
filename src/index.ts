42cb46a384481bc0ef6ae5c6f82bb4431b9c82e6
重田 将人 <masato.0928.j@gmail.com>
2021-09-30 20:37:12 +0900

    eslintがパスするように直した

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-const { WebClient } = require('@slack/web-api');
+import { WebClient } from '@slack/web-api';
+import { Message } from '@slack/web-api/dist/response/GroupsRepliesResponse';
+
 const client = new WebClient(process.env.SLACK_TOKEN);
 
-const getMessageTimestamps = async(): Promise<string[]> => {
+const getMessageTimestamps = async(): Promise<string[] | undefined> => {
   try {
-    const result = await client.conversations.history({
-      channel: process.env.CHANNEL
-    });
-    return result.messages.map(message => message.ts);
+    const result = await client.conversations.history({channel: process.env.CHANNEL || ''});
+
+    if (result.messages == undefined) {
+      return undefined
+    }
+
+    return result.messages.map((message: Message): string => message.ts || '');
   } catch (error) {
     console.log(error)
   }

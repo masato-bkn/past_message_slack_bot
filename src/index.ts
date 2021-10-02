eadbc592c245a53830340ce8f82468a45dbc8989
重田 将人 <masato.0928.j@gmail.com>
2021-10-02 10:57:06 +0900

    メッセージのリンクを取得する関数を作成

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { WebClient } from '@slack/web-api';
 import { Message } from '@slack/web-api/dist/response/GroupsRepliesResponse';
 
+const CHANNEL = process.env.CHANNEL || '';
+
 const client = new WebClient(process.env.SLACK_TOKEN);
 
 const getMessageTimestamps = async(): Promise<string[]> => {
   try {
-    const result = await client.conversations.history({channel: process.env.CHANNEL || ''});
+    const result = await client.conversations.history({channel: CHANNEL});
 
     if (result.messages == undefined) {
       return []
@@ -21,8 +23,13 @@ const getRandomNumber = (max: number) : number=> {
   return Math.floor(Math.random() * max)
 }
 
+const getPermaLink = async(messageTimeStamp: string): Promise<string | undefined> => {
+  const result = await client.chat.getPermalink({channel: CHANNEL, message_ts: messageTimeStamp})
+  return result.permalink;
+}
+
 (async() => {
   const timeStamps = await getMessageTimestamps();
   const i = getRandomNumber(timeStamps.length)
-  console.log(timeStamps[i])
-}) 
+  console.log(await getPermaLink(timeStamps[i]))
+})();

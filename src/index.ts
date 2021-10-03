36704ab48d47718cca772ab0938e54abdad1d9da
重田 将人 <masato.0928.j@gmail.com>
2021-10-03 16:30:00 +0900

    チャンネルにメッセージを送信する

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { WebClient } from '@slack/web-api';
 import { Message } from '@slack/web-api/dist/response/GroupsRepliesResponse';
 
-const CHANNEL = process.env.CHANNEL || '';
+const CHANNEL_ID = process.env.CHANNEL_ID || '';
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 
 const getMessageTimestamps = async(): Promise<string[]> => {
   try {
-    const result = await client.conversations.history({channel: CHANNEL});
+    const result = await client.conversations.history({channel: CHANNEL_ID});
 
     if (result.messages == undefined) {
       return []
@@ -15,7 +15,8 @@ const getMessageTimestamps = async(): Promise<string[]> => {
 
     return result.messages.map((message: Message): string => message.ts || '');
   } catch (error) {
-    return error
+    console.log(`fail: getMessageTimestamps error is ${error}`)
+    return error;
   }
 }
 
@@ -23,13 +24,28 @@ const getRandomNumber = (max: number) : number=> {
   return Math.floor(Math.random() * max)
 }
 
-const getPermaLink = async(messageTimeStamp: string): Promise<string | undefined> => {
-  const result = await client.chat.getPermalink({channel: CHANNEL, message_ts: messageTimeStamp})
-  return result.permalink;
+const getPermaLink = async(messageTimeStamp: string): Promise<string> => {
+  try {
+    const result = await client.chat.getPermalink({channel: CHANNEL_ID, message_ts: messageTimeStamp})
+    return result.permalink || '';
+  } catch (error) {
+    console.log("fail: getPermaLink error is ${error}")
+    return error;
+  }
+}
+
+const postMessageLink = async (link: string): Promise<void> => {
+  try {
+    await client.chat.postMessage({channel: CHANNEL_ID, text: link})
+  } catch (error) {
+    console.log("fail: postMessageLink error is ${error}")
+    console.log(error)
+  }
 }
 
 (async() => {
   const timeStamps = await getMessageTimestamps();
   const i = getRandomNumber(timeStamps.length)
-  console.log(await getPermaLink(timeStamps[i]))
+  const link = await getPermaLink(timeStamps[i])
+  await postMessageLink(link)
 })();

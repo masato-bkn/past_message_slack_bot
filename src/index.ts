3cd1786f2d842cf5cf58f1d1f3edcfa26de88d8d
重田 将人 <masato.0928.j@gmail.com>
2021-10-05 20:17:23 +0900

    prettierの導入

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,65 @@
-import { WebClient } from '@slack/web-api';
-import { Message } from '@slack/web-api/dist/response/GroupsRepliesResponse';
+import { WebClient } from "@slack/web-api";
+import { Message } from "@slack/web-api/dist/response/GroupsRepliesResponse";
 
-const MESSASGE_GET_CHANNEL_ID = process.env.MESSASGE_GET_CHANNEL_ID || '';
-const MESSASGE_POST_CHANNEL_ID = process.env.MESSASGE_POST_CHANNEL_ID || '';
+const MESSASGE_GET_CHANNEL_ID = process.env.MESSASGE_GET_CHANNEL_ID || "";
+const MESSASGE_POST_CHANNEL_ID = process.env.MESSASGE_POST_CHANNEL_ID || "";
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 
-const getMessageTimestamps = async(): Promise<string[]> => {
+const getMessageTimestamps = async (): Promise<string[]> => {
   try {
-    const result = await client.conversations.history({channel: MESSASGE_GET_CHANNEL_ID});
+    const result = await client.conversations.history({
+      channel: MESSASGE_GET_CHANNEL_ID,
+    });
 
     if (result.messages == undefined) {
-      return []
+      return [];
     }
 
-    return result.messages.map((message: Message): string => message.ts || '');
+    return result.messages.map((message: Message): string => message.ts || "");
   } catch (error) {
-    console.log(`fail: getMessageTimestamps error is ${error}`)
+    console.log(`fail: getMessageTimestamps error is ${error}`);
     return error;
   }
-}
+};
 
-const getRandomNumber = (max: number) : number=> {
-  return Math.floor(Math.random() * max)
-}
+const getRandomNumber = (max: number): number => {
+  return Math.floor(Math.random() * max);
+};
 
-const getPermaLink = async(messageTimeStamp: string): Promise<string> => {
+const getPermaLink = async (messageTimeStamp: string): Promise<string> => {
   try {
-    const result = await client.chat.getPermalink({channel: MESSASGE_GET_CHANNEL_ID, message_ts: messageTimeStamp})
-    return result.permalink || '';
+    const result = await client.chat.getPermalink({
+      channel: MESSASGE_GET_CHANNEL_ID,
+      message_ts: messageTimeStamp,
+    });
+    return result.permalink || "";
   } catch (error) {
-    console.log("fail: getPermaLink error is ${error}")
+    console.log("fail: getPermaLink error is ${error}");
     return error;
   }
-}
+};
 
 const postMessageLink = async (link: string): Promise<void> => {
   try {
-    await client.chat.postMessage({channel: MESSASGE_POST_CHANNEL_ID, text: link})
+    await client.chat.postMessage({
+      channel: MESSASGE_POST_CHANNEL_ID,
+      text: link,
+    });
   } catch (error) {
-    console.log("fail: postMessageLink error is ${error}")
-    console.log(error)
+    console.log("fail: postMessageLink error is ${error}");
+    console.log(error);
   }
-}
+};
 
-(async() => {
+(async () => {
   const timeStamps = await getMessageTimestamps();
 
-  let i = 0 
+  let i = 0;
   while (i < 5) {
-    const index = getRandomNumber(timeStamps.length)
-    const link = await getPermaLink(timeStamps[index])
-    await postMessageLink(link)
-    i += 1
+    const index = getRandomNumber(timeStamps.length);
+    const link = await getPermaLink(timeStamps[index]);
+    await postMessageLink(link);
+    i += 1;
   }
 })();
